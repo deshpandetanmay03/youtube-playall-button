47e76a18b1000d250d07708ea31d4a32994e7c55
tanmay <deshpandetanmay2003@gmail.com>
2024-06-02 22:38:37 +0530

    removed firefox no intentions of supporting it

--- firefox/background.js
+++
@@ -1,9 +0,0 @@
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/@")) {
-    const url = tab.url;
-    browser.tabs.sendMessage(tabId, {
-      type: "opened_channel",
-      url: url,
-    });
-  }
-});
