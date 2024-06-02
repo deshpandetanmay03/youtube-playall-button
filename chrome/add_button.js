dc6480ce6ce4d0b21ec930cffcc1f88a520965a4
tanmay <deshpandetanmay2003@gmail.com>
2024-06-02 22:37:02 +0530

    code was never meant to be async but i made it happen anyway
    im the king of the world

--- chrome/add_button.js
+++ chrome/add_button.js
@@ -1,42 +1,57 @@
-(() => {
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const {type, url} = obj;
-        if (type == "opened_channel" && url.endsWith("videos")) {
-            const req = `https://tanmays-helpers.vercel.app/get_playlist?url=${url}`;
-            fetch(req)
-                .then(response => response.json())
-                .then(data => {
-                    let parentElement = document.querySelectorAll('#chips');
-                    while (!parentElement) {
-                        parentElement = document.querySelectorAll('#chips');
-                    }
-                    const button = document.createElement("a");
-                    button.href = data["url"];
-                    button.target = "_";
-                    button.textContent = "Playlist";
-                    button.style = `
-                    margin-left: 12px;
-                    background-color: var(--yt-spec-badge-chip-background);
-                    color: var(--yt-spec-text-primary);
-                    border-radius: 8px;
-                    min-width: 12px;
-                    transition: background-color 0.5s cubic-bezier(0.05, 0, 0, 1) 0s;
-                    padding: 0 var(--ytd-margin-3x);
-                    display: inline-block;
-                    text-decoration: none;
-                    font-family: Roboto, Arial, sans-serif;
-                    padding-block: 6px;
-                    font-size: 1.4rem;
-                    line-height: 2rem;
-                    font-weight: 500;
-                    vertical-align: top;`;
-                    parentElement.forEach(element => {
-                        element.appendChild(button);
-                    });
-                })
-                .catch(error => {
-                    console.error("CHANNEL PLAYLIST - ", error);
-                });
-        }
-    })
-})()
+function createButton(url) {
+    const button = document.createElement("a");
+    button.href = url;
+    button.target = "_";
+    button.textContent = "Playlist";
+    button.style = `
+    margin-left: 12px;
+    background-color: var(--yt-spec-badge-chip-background);
+    color: var(--yt-spec-text-primary);
+    border-radius: 8px;
+    min-width: 12px;
+    transition: background-color 0.5s cubic-bezier(0.05, 0, 0, 1) 0s;
+    padding: 0 var(--ytd-margin-3x);
+    display: inline-block;
+    text-decoration: none;
+    font-family: Roboto, Arial, sans-serif;
+    padding-block: 6px;
+    font-size: 1.4rem;
+    line-height: 2rem;
+    font-weight: 500;
+    vertical-align: top;`;
+    return button;
+}
+async function fetchPlaylist() {
+    const req = `https://tanmays-helpers.vercel.app/get_playlist?url=${window.location.href}`;
+    const response = await fetch(req);
+    const data = await response.json();
+    return data["url"];
+}
+async function getPlaylistUrlWithCache(channelName) {
+    const result = await chrome.storage.sync.get([channelName]);
+    if (Object.keys(result).length === 0) {
+        const playlistUrl = await fetchPlaylist();
+        chrome.storage.sync.set({[channelName]: playlistUrl});
+        return playlistUrl;
+    } else {
+        const playlistUrl = result[channelName];
+        return playlistUrl;
+    }
+}
+chrome.runtime.onMessage.addListener(async (obj, sender, response) => {
+    const {type, channelName} = obj;
+    const url = window.location.href;
+
+    if (type != "opened_channel" || !url.endsWith("videos")) return;
+
+    const playlistUrl = await getPlaylistUrlWithCache(channelName)
+
+    let parentElement = document.querySelectorAll('#chips');
+    while (!parentElement) {
+        parentElement = document.querySelectorAll('#chips');
+    }
+    const button = createButton(playlistUrl);
+    parentElement.forEach(element => {
+        element.appendChild(button);
+    });
+})
--- chrome/background.js
+++ chrome/background.js
@@ -1,9 +1,12 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/@")) {
-    const url = tab.url;
-    chrome.tabs.sendMessage(tabId, {
-      type: "opened_channel",
-      url: url,
-    });
-  }
+    if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/@")) {
+        const url = tab.url;
+        const regex = /@(.*)\//;
+        const match = url.match(regex);
+        const channelName = match[1];
+        chrome.tabs.sendMessage(tabId, {
+            type: "opened_channel",
+            channelName: channelName,
+        });
+    }
 });
